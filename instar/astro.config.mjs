0c39be4259b39fee1f1c0babfe449295e827ffa6
Archit Kumar <architkumar1712@gmail.com>
2022-06-28 19:54:46 +0530

    trying vercel adapter

--- instar/astro.config.mjs
+++ instar/astro.config.mjs
@@ -1,16 +1,17 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static'
 import tailwind from '@astrojs/tailwind';
 import svelte from '@astrojs/svelte'
 import astro from "astro-robots-txt";
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+	adapter: vercel(),
 	site: 'https://instar.vercel.app',
 	experimental: {
 	  integrations: true,
 	},
-  integrations: [tailwind(), astro(),svelte(),react()],
+  integrations: [tailwind(), astro(),svelte()],
   vite:{
 	ssr:{
 		external:["svgo"]
